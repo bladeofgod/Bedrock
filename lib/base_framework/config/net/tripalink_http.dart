c5b4767e228a915345d69496ef92a30d6eb88bd8
bladeofgods <bladeofgods@163.com>
2020-05-18 16:06:54 +0800

    adjust http

--- lib/base_framework/config/net/tripalink_http.dart
+++ lib/base_framework/config/net/tripalink_http.dart
@@ -5,32 +5,25 @@ import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:flustars/flustars.dart';
 import 'package:flutter/foundation.dart';
-import 'package:tripalink/base_framework/config/net/base_http.dart';
-import 'package:tripalink/base_framework/config/storage_manager.dart';
-import 'package:tripalink/base_framework/exception/un_authorized_exception.dart';
-import 'package:tripalink/base_framework/exception/un_caught_exception.dart';
-import 'package:tripalink/base_framework/exception/user_unbind_exception.dart';
-import 'package:tripalink/base_framework/exception/verification_code_exception.dart';
+import 'package:flutter_bedrock/base_framework/config/net/base_http.dart';
+import 'package:flutter_bedrock/base_framework/exception/un_authorized_exception.dart';
+import 'package:flutter_bedrock/base_framework/exception/un_caught_exception.dart';
+import 'package:flutter_bedrock/base_framework/exception/user_unbind_exception.dart';
+
 
 final TripaLinkHttp tripaLinkHttp = TripaLinkHttp();
 
 
 class TripaLinkHttp extends BaseHttp{
 
-  String cookie;
-
-  final CookieManager cookieManager = CookieManager(
-      PersistCookieJar(dir: StorageManager.temporaryDirectory.path)
-  );
 
   @override
   void init() {
-    //temp http://47.92.252.37/index.php?r=test/index
-    //http://47.92.252.37/index.php?r=route/get-online
-    options.baseUrl = "https://apitripalink.com/";
+
+    options.baseUrl = "https://www.baidu.com";
     interceptors
-      ..add(ApiInterceptor())
-      ..add(cookieManager);
+      ..add(CookieManager(PersistCookieJar()))
+      ..add(ApiInterceptor());
   }
 
 }
@@ -41,18 +34,6 @@ class ApiInterceptor extends InterceptorsWrapper{
 
   @override
   Future onRequest(RequestOptions options) async{
-
-    //log();
-//    debugPrint("------------cookie");
-//    debugPrint("${CookieManager.getCookies(tripaLinkHttp.cookieManager.cookieJar.loadForRequest(options.uri))}");
-//    debugPrint("------------cookie");
-  if(tripaLinkHttp.cookie == null || tripaLinkHttp.cookie.isEmpty){
-    String cookie = SpUtil.getString("cookie");
-    options.headers["cookie"] = cookie;
-  }else{
-    options.headers["cookie"] = tripaLinkHttp.cookie??"";
-  }
-
 
     debugPrint('---api-request--->url--> ${options.baseUrl}${options.path}' +
         ' queryParameters: ${options.queryParameters}'
@@ -69,29 +50,10 @@ class ApiInterceptor extends InterceptorsWrapper{
     return options;
   }
 
-  log(){
-
-//    tripaLinkHttp.interceptors.forEach((inter){
-//      debugPrint("${inter.toString()}");
-//    });
-  //tripaLinkHttp.cookieManager.);
-  }
-
 
   @override
   Future onResponse(Response response) {
-    //if(response == null || response.data == null) return null;
-//    debugPrint("------------cookie");
-//    debugPrint("${CookieManager.getCookies(tripaLinkHttp.cookieManager.cookieJar.loadForRequest(response.realUri))}");
-//    debugPrint("------------cookie");
-    //debugPrint("response header  :  ${response.headers.toString()}");
-    //log();
-    if(response.headers != null){
-      if(response.headers.map["set-cookie"] != null && response.headers.map["set-cookie"].toString().isNotEmpty){
-        tripaLinkHttp.cookie = response.headers.map["set-cookie"].toString();
-        SpUtil.putString("cookie", tripaLinkHttp.cookie);
-      }
-    }
+
     ResponseData responseData = ResponseData.fromJson(response.data);
     if(responseData.success){
       return tripaLinkHttp.resolve(responseData);
