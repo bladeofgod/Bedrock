64adeb49d87d73adc020c64c6834b4c69e98cb6f
bladeofgods <bladeofgods@163.com>
2020-06-04 17:33:25 +0800

    working second page... add fake image

--- lib/page/demo_page/main/fake_constant.dart
+++ lib/page/demo_page/main/fake_constant.dart
@@ -6,6 +6,19 @@
 
 class FakeConstant{
 
+  List<String> imageList = [
+    'http://img.08087.cc/uploads/20190113/19/1547380421-DtxGodwgny.jpeg',
+    'http://img.08087.cc/uploads/20190113/19/1547380421-yuCqefakQB.jpg',
+    'http://img.08087.cc/uploads/20200526/21/1590499015-bRKMhvSkAg.jpg',
+    'http://img.08087.cc/uploads/20200526/21/1590498931-psqTFECXur.jpeg',
+    'http://img.08087.cc/uploads/20200526/21/1590498929-OoCIMHjEBp.jpeg',
+    'http://img.08087.cc/uploads/20200526/21/1590498946-jcZriBdqzh.jpg',
+    'http://img.08087.cc/uploads/20200526/21/1590499034-tYUnQzGfKo.jpg',
+    'http://img.08087.cc/uploads/20200526/21/1590498949-PXmwDVKOeG.jpeg',
+    'http://img.08087.cc/uploads/20200526/21/1590498949-CgekfvNcjA.jpg',
+    'http://img.08087.cc/uploads/20200526/21/1590498492-CaVcoyiJFd.jpg',
+  ];
+
 }
 
 
