fbcedd85e1fd302df3cc4426da9c6a6b66428dda
bladeofgods <bladeofgods@163.com>
2020-06-04 10:26:26 +0800

    add unknow page

---
+++ lib/page/demo_page/exception/unknow_page.dart
@@ -0,0 +1,57 @@
+/*
+* Author : LiJiqqi
+* Date : 2020/6/4
+*/
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bedrock/base_framework/widget_state/base_state.dart';
+
+class UnKnowPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return UnKnowPageState();
+  }
+
+}
+
+class UnKnowPageState extends BaseState<UnKnowPage> {
+
+  String introduce = "路由未找到会进入此页面。\n "
+      "理论上很难出现，但是对于一些路由路径后端控制，进行动态内部页面跳转的时候，还是有几率出现的，在此设置有一个页面，用户体验好一些。";
+
+  @override
+  Widget build(BuildContext context) {
+    return switchStatusBar2Dark(child: Container(
+      color: Colors.white,
+      width: getWidthPx(750),height: getHeightPx(1334),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(introduce,style: TextStyle(color: Colors.black,fontSize: getSp(28)),),
+        ],
+      ),
+    ));
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
