8b622c3ae75ae5ce80a00b6c1d5021efd5c7639a
bladeofgods <bladeofgods@163.com>
2020-06-15 14:11:10 +0800

    add mmkv data cache

---
+++ lib/base_framework/view_model/interface/cache_data_factory.dart
@@ -0,0 +1,36 @@
+/*
+* Author : LiJiqqi
+* Date : 2020/6/15
+*/
+
+abstract class CacheDataFactory{
+  cacheData(String data);
+  String fetchCacheData();
+
+  cacheListData(List<String> list);
+  List<String> fetchListCacheData();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
