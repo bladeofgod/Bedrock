130547a2f13d0fdd1ef9ba3e16aaef185df4ea0e
bladeofgod <450103187@qq.com>
2020-07-05 15:39:47 +0800

    ui

---
+++ lib/page/demo_page/main/first/update_page.dart
@@ -0,0 +1,44 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bedrock/base_framework/widget_state/base_state.dart';
+
+class UpdatePage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return UpdatePageState();
+  }
+  
+}
+
+class UpdatePageState extends BaseState<UpdatePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
