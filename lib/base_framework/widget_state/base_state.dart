212bb411c191ccde3dae89cfe08bea794cbb45ba
bladeofgod <450103187@qq.com>
2020-08-09 16:00:26 +0800

    fix bug

--- lib/base_framework/widget_state/base_state.dart
+++ lib/base_framework/widget_state/base_state.dart
@@ -6,10 +6,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bedrock/base_framework/ui/behavior/over_scroll_behavior.dart';
 import 'package:flutter_bedrock/base_framework/utils/image_helper.dart';
+import 'package:oktoast/oktoast.dart';
 
 
 
-abstract class BaseState<T extends StatefulWidget> extends State<T> with RouteAware {
+abstract class BaseState<T extends StatefulWidget> extends State<T>  {
 
   double marginLeft = 0.0;
   double dragPosition = 0.0;
@@ -84,7 +85,7 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> with RouteAw
 
 
   ///去掉 scroll view的 水印  e.g : listView scrollView
-  ///
+  ///当滑动到顶部或者底部时，继续拖动出现的蓝色水印
   Widget getNoInkWellListView({@required Widget scrollView}){
     return ScrollConfiguration(
       behavior: OverScrollBehavior(),
@@ -99,7 +100,6 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> with RouteAw
     return Container(
       width: getWidthPx(750),
       height: getHeightPx(115),
-      //padding: EdgeInsets.only(left: getWidthPx(40),right: getWidthPx(40)),
       color: bgColor??Color.fromRGBO(241, 241, 241, 1),
       padding: EdgeInsets.only(bottom: getHeightPx(10),left: leftPadding,right: rightPadding),
       alignment: Alignment.bottomCenter,
@@ -128,10 +128,16 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> with RouteAw
     );
   }
 
+  ///通用APP bar 统一后退键
   Widget buildAppBarLeft(){
     return GestureDetector(
       onTap: (){
-        Navigator.of(context).pop();
+        if(Navigator.of(context).canPop()){
+          Navigator.of(context).pop();
+        }else{
+          ///增加需要的提示信息
+        }
+
       },
       child: Container(
         color: Colors.white,
@@ -143,43 +149,6 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> with RouteAw
     );
   }
 
-  Widget buildMsgWidget({bool showDot = false,Function hitMsgWidget}){
-    return GestureDetector(
-      onTap: (){
-        if(hitMsgWidget != null){
-          hitMsgWidget();
-        }
-      },
-      child: Container(
-        width: getWidthPx(38),
-        height: getHeightPx(34),
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              bottom: 0,
-              child: Image.asset(ImageHelper.wrapAssetsIcon(showDot? "icon_msg_new.png":"icon_msg.png"),
-                width: getWidthPx(36),height:showDot ? getWidthPx(34):getWidthPx(28),),
-            ),
-            ///dot
-//            Positioned(
-//              right: 0,
-//              top: 0,
-//              child: Visibility(
-//                visible: showDot,
-//                child: ClipOval(
-//                  child: Container(
-//                    width: getWidthPx(14),
-//                    height: getWidthPx(14),
-//                    color: Color.fromRGBO(232, 97, 97, 1),
-//                  ),
-//                ),
-//              ),
-//            ),
-          ],
-        ),
-      ),
-    );
-  }
 
   ///占位widget
   Widget getSizeBox({double width = 1,double height = 1}){
@@ -219,10 +188,7 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> with RouteAw
 //  }
 
 
-  ///使用common app bar时 应该实现下面的方法。
-  leftAppBarWidgetClick(){}
 
-  rightAppBarWidgetClick(){}
 
   /*
   * size adapter with tool ScreenUtil
@@ -254,7 +220,7 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> with RouteAw
   ///  1、webview_flutter ^0.3.21
   ///     不支持html富文本、网页加载导致滚动异常 (如果遇到，使用wrapWebView() 方法可以解决)
   ///  2、flutter_webview_plugin  ^0.3.11
-  ///     支持html富文本、但是会有web浮层导致遮盖其他原生页面（退出页面时即可发现）
+  ///     支持html富文本、但是会有web浮层导致遮盖其他原生页面（滑动退出页面时即可发现）
 
 
   ///解决 webview_flutter 滚动问题 update:解决不了
@@ -270,16 +236,5 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> with RouteAw
 //    );
 //  }
 
-
-  /*
-  * input
-  * 输入内容白名单
-  * */
-  WhitelistingTextInputFormatter getWhiteInputFormatter(RegExp regExp){
-    return WhitelistingTextInputFormatter(regExp);
-  }
-  LengthLimitingTextInputFormatter getLengthInputFormatter(int length){
-    return LengthLimitingTextInputFormatter(length);
-  }
 
 }
