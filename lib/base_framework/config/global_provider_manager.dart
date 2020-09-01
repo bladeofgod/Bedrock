648894cbbdb8c5520955efd77d2e36fa9f11737d
bladeofgods <bladeofgods@163.com>
2020-09-01 15:12:12 +0800

    fix bugs

--- lib/base_framework/config/global_provider_manager.dart
+++ lib/base_framework/config/global_provider_manager.dart
@@ -31,7 +31,7 @@ List<SingleChildWidget> independentServices = [
   //global app cache model
   ChangeNotifierProvider<AppCacheModel>.value(value: AppCacheModel()),//项目缓存model
   //app 状态
-  ChangeNotifierProvider<AppStatusModel>.value(value: AppStatusModel()),
+  //ChangeNotifierProvider<AppStatusModel>.value(value: AppStatusModel()),
 //  ///这里应该放入一个购物车
 //  ChangeNotifierProvider<GlobalCartGoodsModel>.value(value:
 //  GlobalCartGoodsModel()),
