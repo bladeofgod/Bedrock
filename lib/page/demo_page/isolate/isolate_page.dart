e6ca2ddc424662f07d04449133e3c6226a8eac26
bladeofgods <bladeofgods@163.com>
2020-09-02 14:38:13 +0800

    清除 无用代码

--- lib/page/demo_page/isolate/isolate_page.dart
+++ lib/page/demo_page/isolate/isolate_page.dart
@@ -10,7 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bedrock/base_framework/config/net/bedrock_http.dart';
 import 'package:flutter_bedrock/base_framework/utils/isolate/app_private_ioslate.dart';
 import 'package:flutter_bedrock/base_framework/utils/isolate/factory/worker_isolate.dart';
-import 'package:flutter_bedrock/base_framework/utils/isolate/isolate_pool_proxy.dart';
+
 import 'package:flutter_bedrock/base_framework/widget_state/base_state.dart';
 
 class IsolatePage extends StatefulWidget{
