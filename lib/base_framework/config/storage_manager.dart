d26418b0196f09cba8ae48461cb19561d273ae3c
larry <liaoyiping@ibantang.com>
2020-11-20 11:03:41 +0800

    feat：fix getExternalStoragePath 在iOS平台上报错；这是android特有API

--- lib/base_framework/config/storage_manager.dart
+++ lib/base_framework/config/storage_manager.dart
@@ -1,32 +1,27 @@
-
-
-
 import 'dart:io';
 
 import 'package:flustars/flustars.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:path_provider/path_provider.dart';
 
-
-class StorageManager{
-
-
+class StorageManager {
   /// 临时目录  （华为手机可能会随机删除这个目录下的文件）
   static Directory temporaryDirectory;
+
   ///应用目录
   static Directory appDirectory;
+
   ///外部存储 (仅限安卓)
   static Directory externalDirectory;
 
-  static init()async{
+  static init() async {
     await SpUtil.getInstance();
     temporaryDirectory = await getTemporaryDirectory();
     appDirectory = await getApplicationDocumentsDirectory();
-    externalDirectory = await getExternalStorageDirectory();
+    if (Platform.isAndroid) {
+      externalDirectory = await getExternalStorageDirectory();
+    }
+
     ///本地缓存基本都可以使用此工具
     ///后续页面可以同步使用
-
-
   }
-
 }
