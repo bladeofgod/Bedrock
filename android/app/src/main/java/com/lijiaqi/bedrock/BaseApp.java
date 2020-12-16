e2d9742d5ff37f131430e0f8dc9406071e85986f
bladeofgods <bladeofgods@163.com>
2020-12-16 11:38:06 +0800

    update 1.0.22

--- android/app/src/main/java/com/lijiaqi/bedrock/BaseApp.java
+++ android/app/src/main/java/com/lijiaqi/bedrock/BaseApp.java
@@ -20,11 +20,11 @@ public class BaseApp extends FlutterApplication {
         //  混合项目的话，可以考虑开启
         AndroidPlatformProtect.getInstance(new DefaultActivityExceptionHandler())
                 ///处理UI线程的异常
-                .protectUIThread()
+                //.protectUIThread()
                 ///处理 activity启动时的异常
                 //注释原因： <activity启动保护> 功能，使用了反射。
                 //          在混淆下可能会无效或者出现无法预知的问题，建议开启后反复测试
-                .protectActivityStart()
+                //.protectActivityStart()
                 ///处理子线程异常
                 .protectChildThread()
                 .init(this);
