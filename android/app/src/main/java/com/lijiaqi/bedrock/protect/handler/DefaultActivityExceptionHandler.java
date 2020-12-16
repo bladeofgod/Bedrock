68acd456466a05fbce12c22b7c83aea0af5ffcfc
bladeofgods <bladeofgods@163.com>
2020-12-16 11:35:14 +0800

    增加activity 异常善后类..测试

--- android/app/src/main/java/com/lijiaqi/bedrock/protect/handler/DefaultActivityExceptionHandler.java
+++ android/app/src/main/java/com/lijiaqi/bedrock/protect/handler/DefaultActivityExceptionHandler.java
@@ -7,11 +7,15 @@ package com.lijiaqi.bedrock.protect.handler;
  */
 
 import android.app.Activity;
+import android.util.Log;
 
 public class DefaultActivityExceptionHandler extends ActivityExceptionHandler {
-    ///[protectUIThread]和[protectActivityStart]异常将会回调这个方法
+    private final String TAG = "ExceptionHandler";
+    ///[protectUIThread]和[protectActivityStart]异常将会调用这个方法
     @Override
     public void onException(Activity activity, Exception e) {
+        Log.d(TAG,"==========Activity(UI thread) 发生了一个异常=========");
+        if(e != null)Log.d(TAG,e.getMessage());
         if(activity != null) activity.finish();
     }
 }
