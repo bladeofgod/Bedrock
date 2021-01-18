93ff7e77fdef6676571c3d904453e92811cf4b19
bladeofgods <bladeofgods@163.com>
2021-01-18 11:44:23 +0800

    补全 net_observer的注释

--- lib/base_framework/observe/app_status/net_observer.dart
+++ lib/base_framework/observe/app_status/net_observer.dart
@@ -12,10 +12,22 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
+
+/*
+* 注意，此方法在child_isolate
+* 返回值为List
+* */
+
+/// 第一个元素等于此key，则第二个元素为 child isolate 的sendPort
 const String kNetPortKey = 'kNetPortKey';
 
+///第一个元素为[kNetAvailable]
 const String kNetAvailable = 'kNetAvailable';
+///第二个元素则为下面的其中之一
 const String kNetEnable = 'kEnable',kNetDisable = 'kDisable';
+
+
+const int intervalTime = 10;
 
 void observerNetState(SendPort sendPort){
   final String china = 'baidu.com';
@@ -29,7 +41,7 @@ void observerNetState(SendPort sendPort){
 
 
 
-  final timer = Timer.periodic(Duration(seconds: 10), (timer) async{
+  final timer = Timer.periodic(Duration(seconds: intervalTime), (timer) async{
     try{
       ///注意区分国内外，或者用你自己的
       String host = china;
